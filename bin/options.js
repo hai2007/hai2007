6f8c854599513dbe2774a1493c84658f0c38b1ba
你好2007 <2501482523@qq.com>
2021-09-12 11:30:34 +0800

    新增网络和帮助功能

---
+++ bin/options.js
@@ -0,0 +1,47 @@
+module.exports = {
+
+    // 可用命令
+    "command": {
+        create: {
+            info: "创建常用项目脚手架",
+            demo: "hai2007 create",
+            config: ['force']
+        },
+        network: {
+            info: "查看网络相关信息",
+            demo: "hai2007 network",
+            config: []
+        }
+    },
+
+    // 参数配置
+    "config": {
+        force: {
+            short: 'f',
+            info: '强制执行',
+            demo: '--force|-f'
+        },
+        version: {
+            short: 'v',
+            info: '打印版本号',
+            demo: '--version|-v'
+        },
+        help: {
+            short: 'h',
+            info: '显示帮助学习',
+            demo: '--help|-h <term>'
+        },
+    },
+
+    // 帮助信息
+    "help": `
+    Usage: hai2007 <command>
+    
+    where <command> is one of:
+      create, network
+    
+    hai2007 --help|-h <命令>       打印具体的命令说明
+    hai2007 --help|-h              打印所有可用命令
+      `
+
+};
